cc1dd59ca4a7a87f4f07373f002a95ea100032a6
Angel Sebastian Saccone Graneros <saccone.sebastian@gmail.com>
2024-10-17 00:07:58 -0300

    first commit

---
+++ app/components/particles/navBar.jsx
@@ -0,0 +1,83 @@
+"use client";
+import React, { useState } from 'react';
+import Image from 'next/image';
+
+
+
+const NavBar = () => {
+  const [isClick, setisClick] = useState(false);
+   const toggleNavBar = () =>{
+    setisClick(!isClick)
+   }
+  return (
+    <>
+    <nav className='bg-pink-300'>
+        <div className='max-w-7 mx-auto px4 sm:px6 lg:px-8'>
+            <div className='flex items-center justify-between h-16'>
+                <div className='flex items-center'>
+                    <div className='flex-shrink-0'>
+                     <a href="/">
+                     <img src="/Olivia.png"
+                      width={"100"}
+                      height={"100"}
+                      alt='logo'/></a>
+                    </div>
+                </div>
+                <div className='hidden md:block'>
+                  <div className='ml-4 flex  items-center space-x-4'>
+                    <a href="/" className='text-white hover:bg-pink-500 hover:text-pink-300 rounded-md p-2'>Home</a>
+                    <a href="/" className='text-white hover:bg-pink-500 hover:text-pink-300 rounded-md p-2'>Sales!</a>
+                    <a href="/" className='text-white hover:bg-pink-500 hover:text-pink-300 rounded-md p-2'>Perfil</a>
+                    <a href="/" className='text-white hover:bg-pink-500 hover:text-pink-300 rounded-md p-2'>Carrito</a>
+                    <a href="/" className='text-white hover:bg-pink-500 hover:text-pink-300 rounded-md p-2'>Salir</a>
+                  </div>
+                </div>
+                <div className='md:hidden flex items-center'>
+                  <button
+                  className='inline-fex items-center justify-center p-2 rounded-md text-white hover:text-pink-600 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-pink-300' onClick={toggleNavBar}>
+                    {isClick ? (
+                      <svg
+                      className='h-6 w-6'
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      stroke='currentColor'>
+                        <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth={2}
+                        d='M6 18L18 6M6 6l12 12'/>
+                      </svg>
+                    ):(
+                      <svg className='h-6 w-6'
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      stroke='currentColor'>
+                        <path strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth={2}
+                        d='M4 6h16M4 12h16m-7 6h7'/>
+                      </svg>
+                    )}
+                  </button>
+                </div>
+            </div>
+        </div>
+        {isClick && (
+          <div className='md:hidden'>
+            <div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
+            <a href="/" className='text-white hover:bg-pink-500 hover:text-pink-300 rounded-md p-2'>Home</a>
+                    <a href="/" className='text-white block hover:bg-pink-500 hover:text-pink-300 rounded-md p-2'>Sales!</a>
+                    <a href="/" className='text-white block hover:bg-pink-500 hover:text-pink-300 rounded-md p-2'>Perfil</a>
+                    <a href="/" className='text-white block hover:bg-pink-500 hover:text-pink-300 rounded-md p-2'>Carrito</a>
+                    <a href="/" className='text-white block hover:bg-pink-500 hover:text-pink-300 rounded-md p-2'>Salir</a>
+            </div>
+          </div>
+        )}
+        </nav>
+    </>
+  );
+};
+
+export default NavBar
--- app/page.js
+++ app/page.js
@@ -1,26 +1,27 @@
 import Image from "next/image";
+import {Roboto, Raleway} from "next/font/google";
+
+const raleway = Raleway({
+  subsets:['latin'],
+  weight:'300'
+  });
+  
+  const roboto = Roboto({
+    subsets:['latin'],
+    weight:'300'
+    });
 
 export default function Home() {
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <h1 className={`${raleway} antialiased`}>OLIVIA SHOWROOM</h1>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
+          <li className={`${roboto} antialiased`}>
+            Registrate            
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Enterate de las mejores ofertas!</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
@@ -37,7 +38,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            ingresar
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
@@ -45,7 +46,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            registrate
           </a>
         </div>
       </main>
@@ -63,7 +64,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+         Quienes Somos?
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -78,7 +79,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          Contactanos
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -93,9 +94,10 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Redes sociales
         </a>
       </footer>
     </div>
   );
 }
+
